28085781b646afee52d353d308cdebcdd05722d9
Tech13-08 <falaktulsi@gmail.com>
2025-01-11 17:48:11 -0800

    Integrated model with flask web server and fire risk calculation

--- Backend/app.py
+++ Backend/app.py
@@ -8,12 +8,11 @@ from timezonefinder import TimezoneFinder
 from tensorflow.keras.preprocessing.image import img_to_array
 from PIL import Image, ImageOps
 import numpy as np 
-import sys
 from tensorflow.keras.models import load_model
 
 import json
 import requests
-
+import os
 app = Flask(__name__)
 model = load_model('wildfire_detection_model.h5')
 
@@ -33,6 +32,30 @@ def getForecastFromLatLon(lat, lon):
 
     return filtered_forecast_data
 
+def getFWI(temperature, humidity, wind_speed):
+    ffmc = (temperature - 10) * (100 - humidity) / 100 
+    isi = wind_speed * (temperature / 10)  
+    fwi = ffmc * isi / 100
+    print("Pre FWI: ", fwi)
+    return fwi
+
+def normalize_fwi(score, min_fwi=0, max_fwi=50):
+    if score < min_fwi:
+        return 0.0
+    elif score > max_fwi:
+        return 1.0
+    return (score - min_fwi) / (max_fwi - min_fwi)
+
+def getModelConfidence(file):
+    img = Image.open(file)
+    img = ImageOps.pad(img, size=(128, 128), color=(0, 0, 0)) 
+    img_array = img_to_array(img)
+    img_array = img_array / 255.0 
+    img_array = np.expand_dims(img_array, axis=0)  
+
+    prediction = model.predict(img_array)
+    confidence = (1 - float(prediction[0][0])) *100
+    return confidence
 
 @app.route('/getFullForecastFromLatLon', methods=['GET'])
 def getFullForecastFromLatLon():
@@ -51,84 +74,51 @@ def getCurrentForecastFromLatLon():
         json.dumps(getForecastFromLatLon(lat, lon)[0]).encode('utf-8')
     )
 
-@app.route('/getFWI', methods=['GET'])
-def getFWI():
+@app.route('/getFireRisk', methods=['GET'])
+def getFireRisk():
+    file = "./unchecked_camera_image/image.jpeg"
+    if not os.path.exists(file):
+        return jsonify({'error': 'No file saved'}), 400
+    
     lat = float(request.args.get('lat'))
     lon= float(request.args.get('lon'))
     forecastData = getForecastFromLatLon(lat, lon)[0]
     temperature = float(forecastData["temperature"])
     humidity = float(forecastData["relativeHumidity"])
     wind_speed = float(forecastData["windVector"][0])
-    print(temperature, humidity, wind_speed)
-    ffmc = (temperature - 10) * (100 - humidity) / 100 
-    isi = wind_speed * (temperature / 10)  
-    fwi = ffmc * isi / 100
-    return Response(
-        json.dumps(fwi).encode('utf-8')
-    )
+    fwi = normalize_fwi(getFWI(temperature, humidity, wind_speed)) * 100
 
+    confidence = getModelConfidence(file)
 
-@app.route('/detect-fire', methods=['POST'])
-def detect_fire():
-    # Check if a file is in the request
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file provided'}), 400
+    result = float(f"{(fwi + confidence)/2:.2f}")
 
-    # Get the file from the request
-    file = request.files['file']
-    result = {}
-
-    # Open the image
-    img = Image.open(file)
-    img = ImageOps.pad(img, size=(128, 128), color=(0, 0, 0))  # Resize and pad the image
-    img_array = img_to_array(img)
-    img_array = img_array / 255.0  # Normalize pixel values
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
-
-    # Load the model and make a prediction
-    prediction = model.predict(img_array)
-    confidence = float(prediction[0][0])
-
-    # Generate response based on the prediction
-    if confidence < 0.5:
-        result = {'message': 'Fire detected', 'confidence': (1 - confidence) * 100}
-    else:
-        result = {'message': 'No fire detected', 'confidence': confidence * 100}
+        
+    print(f"FWI: {fwi} \n Confidence: {confidence} \n Result: {result}")
 
     return Response(
         json.dumps(result).encode('utf-8')
     )
 
- 
 
-# @app.route('/detect-fire', methods=['GET'])
-# def detect_fire():
-#     # if 'file' not in request.files:
-#     #     return jsonify({'error': 'No file provided'}), 400
-    
-#     model = load_model('wildfire_detection_model.h5')
-    
-# # file = request.files['file']  # Get the uploaded image
-#     # img = Image.open(file)
-#     image_path = "./grassyhills.jpeg"
-#     img = Image.open(image_path)
-#     img = ImageOps.pad(img, size=(128, 128), color=(0, 0, 0))  
-#     img_array = img_to_array(img)
-#     img_array = img_array / 255.0 # normalize pixel
-#     img_array = np.expand_dims(img_array, axis=0)
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file provided'}), 400
 
-#     prediction = model.predict(img_array)
-#     confidence = prediction[0][0]
-#     print("Prediction:", prediction)
+    file = request.files['file']
+    folder = "./unchecked_camera_image"
+    if os.path.exists(folder):
+        for item in os.listdir(folder):
+            item_path = os.path.join(folder, item)
+            if os.path.isfile(item_path) or os.path.islink(item_path):
+                os.unlink(item_path) 
 
-#     if confidence < 0.5:
-#         print(f"Fire detected with {(1 -confidence) * 100:.2f}% confidence.")
-#     else:
-#         print(f"No fire detected with {confidence * 100:.2f}% confidence.")
-    
-    
-#     return "Checked"
+    file_path = os.path.join(folder, "image.jpeg")
+    file.save(file_path)
 
+    return jsonify({'message': f'File saved to {file_path}'}), 200
+
+    
 
 
 if __name__ == '__main__':
